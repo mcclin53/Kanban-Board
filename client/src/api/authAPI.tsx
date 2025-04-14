0e583de329c4bb0d7018de9b28be45aa291e95c7
mcclin53 <dan.mcclintic@gmail.com>
2025-04-13 22:04:58 -0400

    added authAPI

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,24 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    const data = await response.json();
 
-
+    if(!response.ok) {
+      throw new Error('User info not retreived');
+    }
+    return data;
+  } catch (err) {
+    console.log('Error on user login: ', err);
+    return Promise.reject('Failed to fetch user info');
+  }
+};
 
 export { login };
